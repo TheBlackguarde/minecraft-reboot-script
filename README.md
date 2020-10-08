147ab474c09e53238495bf2b7d6bb41896ae4dd2
Marlon Stoops <marlon.stoops@hotmail.com>
2020-10-08 23:29:57 +0200

    Allow different passwords per server

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Make sure the docker container with the server is set to restart unless-stopped or always.
 
-Call this script from cron with name of the docker container.
+Call this script from cron with the name of the docker container and the rcon password.
 
 e.g
 
 ```bash
-55 1 * * * /usr/local/sbin/restart_mc_server.sh mc_server
+55 1 * * * /usr/local/sbin/restart_mc_server.sh container_name rconpassword
 ```
--- restart_mc_server.sh
+++ restart_mc_server.sh
@@ -5,31 +5,31 @@
 echo "Total Arguments:" $#
 echo "All Arguments values:" $@
 
-docker exec $1 rcon-cli --password test say Server reboot in 5 min
+docker exec $1 rcon-cli --password $2 say Server reboot in 5 min
 sleep 60
-docker exec $1 rcon-cli --password test say Server reboot in 4 min
+docker exec $1 rcon-cli --password $2 say Server reboot in 4 min
 sleep 60
-docker exec $1 rcon-cli --password test say Server reboot in 3 min
+docker exec $1 rcon-cli --password $2 say Server reboot in 3 min
 sleep 60
-docker exec $1 rcon-cli --password test say Server reboot in 2 min
+docker exec $1 rcon-cli --password $2 say Server reboot in 2 min
 sleep 60
-docker exec $1 rcon-cli --password test say Server reboot in 1 min
+docker exec $1 rcon-cli --password $2 say Server reboot in 1 min
 sleep 30
-docker exec $1 rcon-cli --password test say Server reboot in 30 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 30 sec
 sleep 10
-docker exec $1 rcon-cli --password test say Server reboot in 20 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 20 sec
 sleep 10
-docker exec $1 rcon-cli --password test say Server reboot in 10 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 10 sec
 sleep 5
-docker exec $1 rcon-cli --password test say Server reboot in 5 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 5 sec
 sleep 1
-docker exec $1 rcon-cli --password test say Server reboot in 4 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 4 sec
 sleep 1
-docker exec $1 rcon-cli --password test say Server reboot in 3 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 3 sec
 sleep 1
-docker exec $1 rcon-cli --password test say Server reboot in 2 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 2 sec
 sleep 1
-docker exec $1 rcon-cli --password test say Server reboot in 1 sec
+docker exec $1 rcon-cli --password $2 say Server reboot in 1 sec
 sleep 1
 echo 'reboot' $1 | systemd-cat -p info
-docker exec $1 rcon-cli --password test stop
+docker exec $1 rcon-cli --password $2 stop
